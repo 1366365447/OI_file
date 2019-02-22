721f17a09ab9160a970f68ee1dca4d1abc8df2d2
1366365447 <1366365447@qq.com>
2019-02-22 15:19:18 +0800

    luogu1372 submit 1

---
+++ luogu1372_又是毕业季I.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+
+int n, k;
+
+int main() {
+	
+	scanf("%d%d", &n, &k);
+	
+	printf("%d\n", n / k);
+	
+	
+	
+	return 0;
+}
